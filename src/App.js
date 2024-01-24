0bf0ae6c0a6c26b2adcca35415fbff8166d5bca7
Sofia Bargues <barguessofia@gmail.com>
2024-01-24 02:21:05 +0000

    initial card

--- src/App.js
+++ src/App.js
@@ -1,7 +1,121 @@
-import React from 'react';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import {
+  IoMdSunny,
+  IoMdRainy,
+  IoMdCloudy,
+  IoMdThunderstorm,
+  IoMdSearch,
+  IoMdSnow,
+  IoMdCloud,
+} from "react-icons/io";
+
+import {
+  BsCloudHaze2Fill,
+  BsCloudDrizzleFill,
+  BsEye,
+  BsWater,
+  BsThermometer,
+  BsWind,
+} from "react-icons/bs";
+
+import { TbTemperatureCelsius } from "react-icons/tb";
+import { ImSpinner8 } from "react-icons/im";
+
+const APIkey = "ec7f898e177edc0c265db7200e2480fa";
 
 const App = () => {
-  return <div>react app</div>;
+  const [data, setData] = useState(null);
+  const [location, setLocation] = useState("Bucharest");
+
+  useEffect(() => {
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${APIkey}`;
+
+    axios.get(url).then((res) => {
+      setData(res.data);
+    });
+  }, [location]);
+
+  if (!data) {
+    return (
+      <div>
+        <div>
+          <ImSpinner8 className="text-5xl animate-spin" />
+        </div>
+      </div>
+    );
+  }
+  let icon;
+  console.log(data.weather[0].main);
+
+  switch (data.weather[0].main) {
+    case "Clouds":
+      icon = <IoMdCloudy />;
+      break;
+    case "Haze":
+      icon = <BsCloudHaze2Fill />;
+      break;
+    case "Rain":
+      icon = <IoMdRainy />;
+      break;
+    case "Clear":
+      icon = <IoMdSunny />;
+      break;
+    case "Drizzle":
+      icon = <BsCloudDrizzleFill />;
+      break;
+    case "Mist":
+      icon = <IoMdCloud />;
+      break;
+    case "Snow":
+      icon = <IoMdSnow />;
+      break;
+    case "Thunderstorm":
+      icon = <IoMdThunderstorm />;
+      break;
+  }
+  const date = new Date();
+  return (
+    <div
+      className="w-full h-screen bg-gradientBg
+     bg-no-repeat gb-cover bg-center flex flex-col
+      items-center justify-center px-4 lg:px-0"
+    >
+      {/* {form} */}
+      <form>form</form>
+      {/* {card} */}
+      <div className="w-full max-w-[450px] bg-black/20 min-h-[584px] text-white backdrop-blur-[32px ] rounded-[32px] py-12 px-6">
+        <div>
+          {/*card top*/}
+          <div className="flex items-center gap-x-5">
+            {/* {icon} */}
+            <div className="text-[87px]">{icon}</div>
+            <div>
+              {/* {country name} */}
+              <div className="text-2xl font-semibold">
+                {data.name}, {data.sys.country}
+              </div>
+              <div>
+                {date.getUTCDate()}/{date.getUTCMonth() + 1}/
+                {date.getUTCFullYear()}
+              </div>
+            </div>
+          </div>
+          <div>
+            {/* card body */}
+            <div className="my-20">
+              <div>
+                {/* temp */}
+                <div>{parseInt(data.main.temp)}</div>
+              </div>
+            </div>
+          </div>
+          {/* card bottom */}
+          <div>card bottom</div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default App;
